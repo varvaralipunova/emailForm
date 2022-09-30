16745379f235810b348169e43800d297e89a604c
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Air.local>
2022-09-29 21:48:37 -0700

    finish email form with template forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailform';
+  email='' as string;
+
+  constructor() {
+  }
+
+  onSubmit() {
+    console.log(this.email)
+  }
 }
